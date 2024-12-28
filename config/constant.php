4b1217388b4f8b613e18b1a20c35d0ada2ebc2bf
Behram-Khattak <behramkttk9@gmail.com>
2024-12-28 23:00:20 +0500

    Updates

--- config/constant.php
+++ config/constant.php
@@ -2,6 +2,6 @@
 
 return [
     'apikey' => [
-        'server_key' => 'AAAAOkAYomw:APA91bEl45i9bZ9kBnOphf9tqAm9eNfxCJy6JNBCmbAdBnh1_txvJJHVE__hxeNy5ea3Wl4QyqS_cpTk5uHPuW_-pmOOachF1I1FL_IFiMGeKRWD4KxBQBQsD-yFj9welLWNt4UIDylD'
+        'server_key' => env('SERVER_KEY')
       ],
 ];
